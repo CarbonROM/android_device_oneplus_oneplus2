7130887a2040aea80de2c6160d71f00dc2ee7c4c
Mikhail Naganov <mnaganov@google.com>
2016-11-21 15:22:17 -0800

    Add all audio HIDL *-impl libraries to the list of product packages
    As *-impl.so files are loaded via dlopen, they don't get added
    automatically with the audio HAL server.
    
    Test: check files on the device
    Change-Id: I9ec8e0b7883d88f3077fcb425a33f19162b2f1df

--- device.mk
+++ device.mk
@@ -83,7 +83,12 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    tinymix
+    tinymix \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl
+
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
