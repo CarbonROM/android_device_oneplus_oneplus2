61fe63dd8f3b18ab9d1eadb18661419d3d9c25aa
Janis Danisevskis <jdanis@google.com>
2017-01-06 10:39:10 +0000

    Update keymaster hal version
    Bug: 32020919
    Change-Id: I11729b7e2899494a13d73c9507626b26bdb2e6d0

--- device.mk
+++ device.mk
@@ -183,6 +183,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl
+
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/fpc1020.kl:system/usr/keylayout/fpc1020.kl \
