259e1f1b364ee1572ee39a68e1d2c6a3782a1bd2
Eino-Ville Talvala <etalvala@google.com>
2017-01-19 16:12:42 -0800

    Add necessary camera HAL libraries for passthrough HIDL operation
    Test: No regressions in a full camera CTS pass w/HIDL enabled
          in camara service.
    Bug: 30985004
    Change-Id: Ie7206c322db893eacab0ba8e6647c1587fccca42

--- device.mk
+++ device.mk
@@ -108,6 +108,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl \
     camera.msm8994 \
     libshim_camera \
     libshim_ims-camera \
