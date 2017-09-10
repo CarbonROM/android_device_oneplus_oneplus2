LineageOS/android_device_huawei_angler

angler: switch to using fingerprint HIDL interface
Test: enroll, unlock, navigation. Boot with no errors related to fingerprint

Bug: 33199080
Change-Id: Ie07a26c648d991232d94b43498592db8dc17d275

device.mk
@@ -95,10 +95,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     AnglerLayout
 
-# include fingerprintd
+# Fingerprint Sensor
 PRODUCT_PACKAGES += \
-    fingerprintd \
-    android.hardware.biometrics.fingerprint@2.1-impl
+    fingerprint.angler \
+    android.hardware.biometrics.fingerprint@2.1-service
 
 # Delegation for OEM customization
 PRODUCT_OEM_PROPERTIES := \

CarbonROM/android_device_oneplus_oneplus2

device.mk
@@ -150,9 +150,10 @@ PRODUCT_PACKAGES += \
     make_ext4fs \
     setup_fs
 
-# Fingerprint sensor
+# Fingerprint Sensor
 PRODUCT_PACKAGES += \
-    fingerprintd
+    fingerprint.msm8994 \
+    android.hardware.biometrics.fingerprint@2.1-service
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \