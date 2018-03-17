0dc9432384b7e481535edcf81f123771bd88ce15
Brett Montague <bmontagu@gmail.com>
2018-03-16 23:15:30 -0400

    oneplus2: carbon-ify
    Signed-off-by: Brett Montague <bmontagu@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,8 +174,8 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_NO_RPC := true
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
